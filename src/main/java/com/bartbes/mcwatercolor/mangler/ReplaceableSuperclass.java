484ec38ea6256111ef2a28ceebe0f30edc4abf81
Bart van Strien <bart.bes@gmail.com>
2015-07-11 16:10:02 +0200

    Add missing file

---
+++ src/main/java/com/bartbes/mcwatercolor/mangler/ReplaceableSuperclass.java
@@ -0,0 +1,13 @@
+package com.bartbes.mcwatercolor.mangler;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ReplaceableSuperclass
+{
+	String[] value();
+}
